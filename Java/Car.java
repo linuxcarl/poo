3299731351387777f82ebfe21a6d6abe596cf4a1
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-15 14:26:22 -0500

    added constructors by class in java

--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,13 @@
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passanger;
+
+    public Car(String license, Account driver){
+        this.license = license;
+        this.driver = driver;
+    }
 
     public void printData() {
         System.out.println("Car license: "+  license + " driver: "+  driver+ " passangers: "+  passanger );
--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,11 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Car car = new Car();
-        car.license = "ZGB09-23";
-        car.driver = "Carlos Ramirez";
+        Car car = new Car("ZGB09-23",new Account("Carlos Ramirez","RAM-ZD4-2"));
         car.passanger = 5;
         car.printData();
 
-        Car car2 = new Car();
-        car2.license = "SADEW3-23";
-        car2.driver = "Marcela Ramirez";
+        Car car2 = new Car("SADEW3-23", new Account("Marcela Ramirez","RAM-ZD4-3"));
         car2.passanger = 9;
         car2.printData();
     }
