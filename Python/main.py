26bbc6d7c00492954de6728e7ff2d96fc5eaf150
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-15 03:06:28 -0500

    unsed class in Main by python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,15 @@
+from car import Car
 if __name__ == "__main__":
     print("Hello World!")
+
+    car = Car()
+    car.license = "ZGB09-23"
+    car.driver = "Carlos Ramirez"
+    car.passanger = 5
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "Ze209-23"
+    car2.driver = "Max Ramirez"
+    car2.passanger = 8
+    print(vars(car2))
