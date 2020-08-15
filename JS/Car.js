bf757003f7754db4619244d072ed4d089545f357
Carlos Ramirez <carlos@extrasistemas.com>
2020-08-15 16:36:42 -0500

    added type data in class Car property driver = new Account()

--- JS/Car.js
+++ JS/Car.js
@@ -1,15 +1,22 @@
+import { Account } from "./Account.js";
 export class Car {
   id = Number();
   license = String();
-  driver = Object();
+  driver = new Account();
   passenger = Number();
-  constructor(license = String(), driver = Object(), passenger = Number()) {
+  constructor(
+    license = String(),
+    driver = new Account(),
+    passenger = Number()
+  ) {
     this.license = license;
     this.driver = driver;
     this.passenger = passenger;
   }
 
   printDataCar() {
-    console.log(`License: ${this.license}. Driver: ${this.driver.name}. Document: ${this.driver.document}. Passengers: ${this.passenger}`);
+    console.log(
+      `License: ${this.license}. Driver: ${this.driver.name}. Document: ${this.driver.document}. Passengers: ${this.passenger}`
+    );
   }
 }
--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,20 @@ import { Car } from "./Car.js";
 import { Account } from "./Account.js";
 const car = new Car("zdf-23s-23", new Account("Carlos Ramirez", "dr10"), 10);
 console.log(car.printDataCar());
-const car2 = new Car("sdc-3s2-23", new Account("Enrique Ramirez", "CERF-199"), 5);
+
+const car2 = new Car();
+car2.license = "sdc-3s2-23";
+car2.driver = { name:"Enrique Ramirez", document:"CERF-199" };
+car2.passenger = 5;
 console.log(car2.printDataCar());
+
+const car3 = new Car();
+car3.license = "sdc-3s2-23";
+car3.driver = new Account("Maximiliano Ramirez", "MRFX-199" );
+car3.passenger = 5;
+console.log(car3.printDataCar());
+
+const car4 = new Car();
+car4.license = "sdc-3s2-23";
+car4.passenger = 5;
+console.log(car4.printDataCar());
